1038527b5bd53da6fb7e7490b8daa0cab2ebbde1
Chris McClellan <cmcclellau@gmail.com>
2022-07-26 18:12:08 +1000

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+import requests
+
+import json
+
+import pandas as pd
+response_API = requests.get('https://anss-api.dev.asiagate.com/v1/member-states')
+#print(response_API.status_code)
+data = response_API.text
+parse_json = json.loads(data)
+
+print(parse_json)
+
+df = pd.json_normalize(parse_json, record_path=['data'])
+
+pd.set_option('display.max_colwidth', None)
+pd.set_option('display.max_columns', None)
+# print(df)
+
+bardf= df[['attributes.name','attributes.unsd']].copy()
+print(bardf)
+
+st.bar_chart(bardf)
