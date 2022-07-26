717f6ee03bfe3638de4b221496a33f2ce215752f
Chris McClellan <cmcclellau@gmail.com>
2022-07-26 19:12:01 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,6 @@ pd.set_option('display.max_columns', None)
 # bardf= df[['attributes.name','attributes.unsd']].copy()
 # bardf = bardf.set_index('attributes.name')
 
-df.loc[df.shape[0]] = [None, None]
+
 fig = px.bar(df, y='attributes.name', x='attributes.unsd', orientation='h', base=0)
 st.write(fig)
